b9b33fc5518a761318816e6af8934be0fa24fdfb
Natalye <cavalieridev@gmail.com>
2023-12-26 09:12:15 +0100

    add page Profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,34 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+import { Container, Form, Avatar } from "./styles"
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+      <Form>
+        <Avatar>
+          <img 
+          src="https://github.com/natalyecavalieri.png" 
+          alt="foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input
+            id="avatar"
+            type="file"/>
+          </label>
+        </Avatar>
+        <Input placeholder="Nome" type="text" icon={FiUser}></Input>
+        <Input placeholder="E-mail" type="email" icon={FiMail}></Input>
+        <Input placeholder="Senha atual" type="password" icon={FiLock}></Input>
+        <Input placeholder="Nova senha" type="password" icon={FiLock}></Input>
+        <Button title="Salvar"></Button>
+      </Form>
+    </Container>
+  )
+}
